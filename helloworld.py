15e06f171410c6b23d3b83f117ce1d5669b1892e
benna1205 <160953914+benna1205@users.noreply.github.com>
2024-09-08 10:02:57 +0000

    My First Python Programms using String

---
+++ helloworld.py
@@ -0,0 +1,52 @@
+content = " this is my first python program"
+output = content
+#This is the normal output with string
+print("this is normal output:>>",output)
+output2 = len(content)
+#This is the output with len function
+print("this is output with len function:>>",output2)
+output3 = content.upper()
+#This is the output with upper function
+print("this is output with upper function:>>",output3)
+output4 = content.lower()
+#This is the output with lower function
+print("this is output with lower function:>>",output4)
+output5 = content.capitalize()
+#This is the output with capitalize function
+print("this is output with capitalize function:>>",output5)
+output6 = content.find("my")
+#This is the output with find function
+print("this is output with find function:>>",output6)
+output7 = content.replace("my","extra")
+#This is the output with replace function
+print("this is output with replace function:>>",output7)
+output8 = content.split(" ")
+#This is the output with split function
+print("this is output with split function:>>",output8)
+output9 = content.strip()
+#This is the output with strip function
+print("this is output with strip function:>>",output9)
+output10 = content.startswith("Hello")
+#This is the output with startswith function
+print("this is output with startswith function:>>",output10)
+output11 = content.endswith("world")
+#This is the output with endswith function
+print("this is output with endswith function:>>",output11)
+output12 = content.isalpha()
+#This is the output with isalpha function
+print("this is output with isalpha function:>>",output12)
+output13 = content.isalnum()
+#This is the output with isalnum function
+print("this is output with isalnum function:>>",output13)
+output14 = content.isdigit()
+#This is the output with isdigit function
+print("this is output with isdigit function:>>",output14)
+output15 = content.islower()
+#This is the output with islower function
+print("this is output with islower function:>>",output15)
+output16 = content.isupper()
+#This is the output with isupper function
+print("this is output with isupper function:>>",output16)
+output17 = content.istitle()
+#This is the output with istitle function
+print("this is output with istitle function:>>",output17)
